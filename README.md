e54b3e2da01b4954e8154f8ac180421ca8ae55d6
Miguel Castillo <manchagnu@gmail.com>
2016-06-26 09:33:04 -0400

    adjusted parameter name from matcher to predicate

--- README.md
+++ README.md
@@ -228,12 +228,12 @@ isMatch(input, {
 ```
 
 
-#### findAll(input, matcher)
+#### findAll(input, predicate)
 
-Returns an array that contains all the items for which calling the matcher function returns true for when the matcher is a function. Or in the case when the matcher is not a function, whatever isMatch returns true for using matcher as the matching criteria.
+Returns an array that contains all the items for which calling the predicate function returns true for when the predicate is a function. Or in the case when the predicate is not a function, whatever isMatch returns true for using predicate as the matching criteria.
 
 - **@param** { object | array } input - Collection to extract matching items from.
-- **@param** { object | array | string | number | function } matcher - If a function is provided, then that's called and any item for which that returns true to for is included in the result set. Otherwise, isMatch is used to deeply match object structures. Object that match the object structure are included in the result set.
+- **@param** { object | array | string | number | function } predicate - If a function is provided, then that's called and any item for which that returns true to for is included in the result set. Otherwise, isMatch is used to deeply match object structures. Object that match the object structure are included in the result set.
 - **@returns** { array } Collection of items that matched the criteria
 
 
--- src/findAll.js
+++ src/findAll.js
@@ -2,23 +2,23 @@ var types = require("dis-isa");
 var isMatch = require("./isMatch");
 
 /**
- * Returns an array that contains all the items for which calling the matcher function returns
- * true for when the matcher is a function. Or in the case when the matcher is not a function,
- * whatever isMatch returns true for using matcher as the matching criteria.
+ * Returns an array that contains all the items for which calling the predicate function returns
+ * true for when the predicate is a function. Or in the case when the predicate is not a function,
+ * whatever isMatch returns true for using predicate as the matching criteria.
  *
  * @param { object | array } input - Collection of items to search for the first match from.
- * @param { object | array | string | number | function } matcher - If a function is provided,
+ * @param { object | array | string | number | function } predicate - If a function is provided,
  *  then that's called and any item for which that returns true to for is included in the
  *  result set. Otherwise, isMatch is used to deeply match object structures. Object that match
  *  the object structure are included in the result set.
  *
  * @returns { array } Collection of items that matched the criteria
  */
-function findAll(input, matcher) {
+function findAll(input, predicate) {
   var result = [];
-  var cb = types.isFunction(matcher) ?
-    matcher :
-    isMatch.withCriteria(matcher);
+  var cb = types.isFunction(predicate) ?
+    predicate :
+    isMatch.withCriteria(predicate);
 
   for (var item in input) {
     if (input.hasOwnProperty(item) && cb(input[item], item, input)) {
