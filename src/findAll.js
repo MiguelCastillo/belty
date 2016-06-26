8fb500912eb39dfca12a8fce737c146031fe885d
Miguel Castillo <manchagnu@gmail.com>
2016-06-26 01:27:04 -0400

    updated docs in findAll file

--- src/findAll.js
+++ src/findAll.js
@@ -5,10 +5,10 @@ var isMatch = require("./isMatch");
  * Creates an array that contains all the items that match the provided
  * criteria.
  *
- * @param { object | array } input - Collection to extract matching items from.
- * @param { object | array | string | number | function } matcher - Object to
- *  match the shape of items in input. Alternatively, you can pass a function to
- *  customize which items in input should be included in the result set.
+ * @param { object | array } input - Collection of items to search for the first match from.
+ * @param { object | array | string | number | function } matcher - Object to match the shape
+ *  of the items to pull from input. Alternatively, you can pass a function to customize which
+ *  items to pull from input.
  *
  * @returns { array } Collection of items that matched the criteria
  */
